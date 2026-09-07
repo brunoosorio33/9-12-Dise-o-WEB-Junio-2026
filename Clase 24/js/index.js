be54dbac0636c96675def596ab61ddaf589f37d0
brunoosorio33 <bruno.osorio33@alumnos.cei.es>
2026-09-07 11:51:38 +0200

    Add files via upload

---
+++ Clase 24/js/index.js
@@ -0,0 +1,92 @@
+let ticket = true
+
+
+/* Datos
+
+- Strings
+- Number
+- Boolean
+
+*/
+
+
+/* Keywords
+
+- let
+- const
+- var
+- function
+- if /else / else if
+
+/*
+
+/* Objetos 
+
+- console
+- document (DOM)
+
+*/
+
+
+/* Funciones nombradas
+    function nombreFunction() {
+        // mi codigo
+    }
+    
+// Funciones anonimas
+    let miFuncion = function () { 
+        mi codigo   
+    }
+
+// Funciones arrow
+    let miArrow = () => {
+        mi codigo
+    }
+
+// Funciones IIFE
+    (function () {
+        // mi codigo
+    })()
+
+*/
+
+// ---------------------
+
+
+// Condicionales
+
+    if(5 > 6) {
+        // mi codigo
+    } else if (7 > 6) {
+        // mi codigo
+    } else {
+        // mi codigo
+    }
+
+
+// Condicionales ternarias 
+    (5 > 6) ? '' : ''
+    
+
+
+// DOM 
+
+let miElemento = document.getElementById('miTitulo') // -> Nodo
+// document.querySelector('') // -> Nodo
+
+// document.getElementsByClassName('') // -> Node List 
+let misElementos = document.querySelectorAll('') // -> Nodo List
+// document.getElementsByTagName('') // -> Nodo List
+
+miElemento.classList.add('')
+miElemento.classList.toggle('')
+miElemento.classList.remove('')
+
+miElemento.innerText = ''
+miElemento.innerHTML = ''
+
+miElemento.style.color  = 'red'
+miElemento.src = ''
+miElemento.href = ''
+
+
